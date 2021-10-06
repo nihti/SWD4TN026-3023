b172bd464b4c531ea3d1ae916dd051fef73a3bbd
nihti <22195470+nihti@users.noreply.github.com>
2021-10-06 19:30:23 +0300

    Todolist

---
+++ src/components/Todolist.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+
+export default function TodoList() {
+    /**
+     *  Create state that contains todo object: 
+     *  {description: ’’, date: ’’}
+     *  addTodo method: insert todo objects inside todos array
+     *  // Enter-key adds input values todo-list too
+     *  onKeyDown={ e => {e.key==='Enter' && AddTodo()} } 
+     */
+    
+    const [thing, setThing] = useState({date: '', desc: ''});
+    const [todos, setTodos] = useState([]);
+
+    const AddTodo = () => {
+        setTodos([...todos, {
+            date: thing.date,
+            desc: thing.desc
+        }]);
+    }
+
+    return(
+        <div className='right'>
+            <label>Date </label>
+            <input 
+                value={thing.date} 
+                onChange={ e => setThing({...thing, date: e.target.value}) } 
+            />
+            <br/><label>Description </label>
+            <input 
+                value={thing.desc} 
+                onChange={ e => setThing({...thing, desc: e.target.value}) } 
+            /><br/>
+            <button onClick={AddTodo}> Add </button>
+            <table>
+                <tbody>    
+                {
+                    todos.map((todo, i) =>
+                    <tr key={i}>
+                        <td>{todo.date}</td>
+                        <td>{todo.desc}</td>
+                    </tr>
+                    )
+                }
+                </tbody>
+            </table>
+        </div>
+    );
+}
