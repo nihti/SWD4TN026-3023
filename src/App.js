76b66a81a8498f33324dcefddd8477411ebb68ab
nihti <22195470+nihti@users.noreply.github.com>
2021-11-10 16:54:16 +0200

    carlist exercise with remove funtionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import React, {Fragment, useState} from 'react';
 import './App.css';
-import TodoList from './components/TodoList';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AppBar from '@mui/material/AppBar';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
+import Carlist from './components/Carlist';
 
 export default function App() {
 const [value, setValue] = useState('home');
-
-  /**
-   * Create a tab menu using the Material UI. 
-   * The menu has two items Home and My Todos. 
-   * The Home item shows a welcome message. 
-   * The My Todos item shows your todo app from the previous exercise.
-   */
 
   const handleChange = (_, val) => {
     setValue(val);
@@ -23,24 +11,7 @@ const [value, setValue] = useState('home');
 
   return (
     <Fragment>
-      <AppBar position="static">
-        <Tabs 
-          value={value} 
-          onChange={handleChange}
-          textColor="secondary"
-          indicatorColor="secondary"
-          centered
-          >
-          <Tab value="home" label="Home"/>
-          <Tab value="todo" label="Todo-list"/>
-        </Tabs>
-      </AppBar>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <div className="App">
-          { value === 'home' && <h1>HOME</h1> }
-          { value === 'todo' && <TodoList/>   }
-        </div>
-      </LocalizationProvider>
+      <Carlist />
     </Fragment>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import TodoTable from './components/TodoTable';
 
+// Default test
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+// Multiply example
+const multiply = (x, y) => {
+  return x * y; 
+}
+test('test 2 * 3', () => {
+  expect(multiply(2, 3)).toBe(6);
+});
+
+// React link default test that was not present 
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// Table test
+test('renders todotable', () => {
+  const row = [{ desc: 'Go to coffee', date: '24.01.2021' }];
+  render(<TodoTable todos={row} />);
+  const tablecell = screen.getByText(/go to coffee/i);
+  expect(tablecell).toBeInTheDocument();
 });
